503bd6d7373da645ec6d5989f219c994bf6fcf87
DavidWells <hello@davidwells.io>
2020-01-13 16:25:33 -0800

    test

--- plugins/exit-early/index.js
+++ plugins/exit-early/index.js
@@ -3,6 +3,7 @@ module.exports = function exitEarlyPlugin(config) {
     name: 'netlify-plugin-exit-build-early',
     onInit: async ({ utils }) => {
       const { git } = utils
+      console.log('git', git)
       /* Do stuff if files modified */
       if (git.modifiedFiles.length) {
         console.log('Modified files:', git.modifiedFiles)
