2cc60028424699b93e5777b58c7eb1f449b219df
DavidWells <hello@davidwells.io>
2020-01-13 16:33:03 -0800

    go again

--- plugins/exit-early/index.js
+++ plugins/exit-early/index.js
@@ -8,6 +8,10 @@ module.exports = function exitEarlyPlugin(config) {
       if (git.modifiedFiles.length) {
         console.log('Modified files:', git.modifiedFiles)
       }
+      if (git.createdFiles.length) {
+        console.log('Created files:', git.createdFiles)
+      }
+      // Find all html files git status
       const htmlFiles = git.fileMatch('**/*.html')
       console.log('html files git info:', htmlFiles)
 
@@ -15,6 +19,7 @@ module.exports = function exitEarlyPlugin(config) {
         console.log('>> EXIT BUILD BC HTML HAS NOT CHANGED\n')
         process.exit(1)
       }
+      console.log('Continue build because HTML files created or changed', htmlFiles.getKeyedPaths())
     }
   }
 }
