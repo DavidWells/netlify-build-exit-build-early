081a1fe385593fa4d14587b171ce27acdd19f396
DavidWells <hello@davidwells.io>
2020-01-21 14:32:32 -0800

    test

--- plugins/exit-early/index.js
+++ plugins/exit-early/index.js
@@ -5,20 +5,20 @@ module.exports = function exitEarlyPlugin(config) {
       const { git } = utils
       console.log('git', git)
       /* Do stuff if files modified */
-      if (git.modifiedFiles.length) {
-        console.log('Modified files:', git.modifiedFiles)
-      }
-      if (git.createdFiles.length) {
-        console.log('Created files:', git.createdFiles)
-      }
-      // Find all html files git status
-      const htmlFiles = git.fileMatch('**/*.html')
-      console.log('html files git info:', htmlFiles)
-
-      if (!htmlFiles.edited) {
-        console.log('>> EXIT BUILD BC HTML HAS NOT CHANGED\n')
-        process.exit(1)
-      }
+      // if (git.modifiedFiles.length) {
+      //   console.log('Modified files:', git.modifiedFiles)
+      // }
+      // if (git.createdFiles.length) {
+      //   console.log('Created files:', git.createdFiles)
+      // }
+      // // Find all html files git status
+      // const htmlFiles = git.fileMatch('**/*.html')
+      // console.log('html files git info:', htmlFiles)
+      //
+      // if (!htmlFiles.edited) {
+      //   console.log('>> EXIT BUILD BC HTML HAS NOT CHANGED\n')
+      //   process.exit(1)
+      // }
       // console.log('Continue build because HTML files created or changed', htmlFiles.getKeyedPaths())
     }
   }
