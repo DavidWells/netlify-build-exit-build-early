084d1fd0b8e2f69b37737e1c1d1fe1bf622dea43
DavidWells <hello@davidwells.io>
2020-01-24 14:56:34 -0800

    update code

--- plugins/exit-early/index.js
+++ plugins/exit-early/index.js
@@ -1,42 +1,43 @@
 const { inspect } = require('util')
 
-function deepLog(obj) {
-  console.log(inspect(obj, {showHidden: false, depth: null}))
-}
-
 module.exports = function exitEarlyPlugin(config) {
   return {
     name: 'netlify-plugin-exit-build-early',
     onInit: async ({ utils }) => {
-      console.log('utils')
-      deepLog(utils)
+      const { git } = utils
+      console.log('git utils')
+      deepLog(git)
 
-      const { git, run } = utils
-
-      await run.command('echo "hi"')
-
-      /* Do stuff if files modified */
+      /* Check if any files modified since last build */
       if (git.modifiedFiles.length) {
         console.log('Modified files:', git.modifiedFiles)
       }
+      /* Check if any files created since last build */
       if (git.createdFiles.length) {
         console.log('Created files:', git.createdFiles)
       }
-      // Find all html files git status
+
+      /* Find all HTML files & check for git status */
       const htmlFiles = git.fileMatch('**/*.html')
       console.log('html files git info:', htmlFiles)
 
-      // Find all js files git status
+      /* Find all JS & check for git status */
       const jsFiles = git.fileMatch('**/*.js')
       console.log('js files git info:', jsFiles)
 
-      // No JS files added/deleted/changed
+      /* No JS files added/deleted/changed, exit build early */
       if (!jsFiles.edited.length && !htmlFiles.edited.length) {
         console.log('No files we care about changed. Exit early')
         process.exit(0)
       }
+
+      /* Files we care about changed. Continue with build */
       console.log('Continue build because HTML/JS files created, changed or deleted')
       console.log(htmlFiles.edited.concat(jsFiles.edited))
     }
   }
+}
+
+function deepLog(obj) {
+  console.log(inspect(obj, {showHidden: false, depth: null}))
 }
